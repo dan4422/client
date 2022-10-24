a91dadb06f1c2ff4595125aa4163fdfbd9ed9081
Daniel Lee <sdlee095@gmail.com>
2022-10-24 17:45:53 -0400

    pagination feature complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,10 +24,6 @@ function Home() {
   const [search, setSearch] = useState('')
   const [tags, setTags] = useState([])
 
-  useEffect(() => {
-    dispatch(getPosts())
-  },[currentId, dispatch])
-
   const searchPost = () => {
     if (search.trim() || tags) {
       //dispatch action to fetch search post
@@ -76,7 +72,7 @@ function Home() {
           </AppBar>
           <Form currentId={currentId} setCurrentId={setCurrentId}/>
           <Paper elevation={6}>
-            <Pagination />
+            <Pagination page={page}/>
           </Paper>
         </Grid>
       </Grid>
